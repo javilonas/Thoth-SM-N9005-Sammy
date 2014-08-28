c412330c1e6765f823fcff713db6fa15348f0a91
javilonas <admin@lonasdigital.com>
2014-08-28 14:33:15 +0200

    Update TPowerCC

--- ramdisk/res/ext/zswap.sh
+++ ramdisk/res/ext/zswap.sh
@@ -8,6 +8,8 @@
 ZSWAPSIZE=512
 if [ $ZSWAPSIZE -gt 0 ]; then
 echo `expr $ZSWAPSIZE \* 1024 \* 1024` > /sys/devices/virtual/block/vnswap0/disksize
+/sbin/busybox swapoff /dev/block/zram0 > /dev/null 2>&1
+echo 1 > /sys/devices/virtual/block/vnswap0/reset
 /sbin/busybox mkswap /dev/block/vnswap0 > /dev/null 2>&1
 /sbin/busybox swapon /dev/block/vnswap0 > /dev/null 2>&1
 fi
