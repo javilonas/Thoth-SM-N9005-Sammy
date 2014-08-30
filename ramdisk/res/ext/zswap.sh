3b92ee813c3c77093f5e7de7a878d1fac4391259
javilonas <admin@lonasdigital.com>
2014-08-30 09:31:32 +0200

    Fix Zswap

--- ramdisk/res/ext/zswap.sh
+++ ramdisk/res/ext/zswap.sh
@@ -3,12 +3,12 @@
 # zswap
 #
 
-#RAMSIZE=`grep MemTotal /proc/meminfo | awk '{ print \$2 }'`
-#ZSWAPSIZE=$(($RAMSIZE*100))
-ZSWAPSIZE=512
+RAMSIZE=`grep MemTotal /proc/meminfo | awk '{ print \$2 }'`
+ZSWAPSIZE=$(($RAMSIZE*100))
+
 if [ $ZSWAPSIZE -gt 0 ]; then
 echo `expr $ZSWAPSIZE \* 1024 \* 1024` > /sys/devices/virtual/block/vnswap0/disksize
-/sbin/busybox swapoff /dev/block/zram0 > /dev/null 2>&1
+/sbin/busybox swapoff /dev/block/vnswap0 > /dev/null 2>&1
 /sbin/busybox mkswap /dev/block/vnswap0 > /dev/null 2>&1
 /sbin/busybox swapon /dev/block/vnswap0 > /dev/null 2>&1
 fi
