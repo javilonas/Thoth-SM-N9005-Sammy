d9769d0fa92d165b62486052d4522ec1de42c49a
javilonas <admin@lonasdigital.com>
2014-09-01 17:40:12 +0200

    Update Build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -7,7 +7,6 @@ echo "#################### Eliminando Restos ####################"
 echo "#################### Preparando Entorno ####################"
 export KERNELDIR=`readlink -f .`
 export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
-#export USE_SEC_FIPS_MODE=true
 export TOOLBASE="/home/lonas/Kernel_Lonas/Thoth-SM-N9005-Sammy/scripts"
 
 if [ "${1}" != "" ];then
@@ -23,7 +22,6 @@ export REVISION="RTM"
 
 export KBUILD_BUILD_VERSION="1"
 
-# make defconfig
 cp arch/arm/configs/lonas_msm8974_sec_defconfig .config
 
 . $KERNELDIR/.config
@@ -51,36 +49,50 @@ find . -type f -name '*.py' -exec chmod 755 {} \;
 find . -type f -name '*.sh' -exec chmod 755 {} \;
 find . -type f -name '*.pl' -exec chmod 755 {} \;
 
-
 echo "ramfs_tmp = $RAMFS_TMP"
 
-#remove previous ramfs files
+echo "#################### Eliminando build anterior ####################"
+
+find -name '*.ko' -exec rm -rf {} \;
+rm -rf $KERNELDIR/arch/arm/boot/zImage > /dev/null 2>&1
+rm -rf $KERNELDIR/arch/arm/boot/zImage-dtb > /dev/null 2>&1
+
+echo "#################### Update Ramdisk ####################"
+rm -f $KERNELDIR/releasetools/tar/$KERNEL_VERSION-$REVISION$KBUILD_BUILD_VERSION-$VERSION_KL.tar > /dev/null 2>&1
+rm -f $KERNELDIR/releasetools/zip/$KERNEL_VERSION-$REVISION$KBUILD_BUILD_VERSION-$VERSION_KL.zip > /dev/null 2>&1
+cp -f $KERNELDIR/arch/arm/boot/zImage . > /dev/null 2>&1
+cp -f $KERNELDIR/arch/arm/boot/zImage-dtb . > /dev/null 2>&1
+
+
 rm -rf $RAMFS_TMP > /dev/null 2>&1
 rm -rf $RAMFS_TMP.cpio > /dev/null 2>&1
 rm -rf $RAMFS_TMP.cpio.gz > /dev/null 2>&1
-#copy ramfs files to tmp directory
-cp -ax $RAMFS_SOURCE $RAMFS_TMP
-#clear git repositories in ramfs
+rm -rf $KERNELDIR/*.cpio > /dev/null 2>&1
+rm -rf $KERNELDIR/*.cpio.gz > /dev/null 2>&1
+cd $ROOTFS_PATH
+cp -ax $ROOTFS_PATH $RAMFS_TMP
 find $RAMFS_TMP -name .git -exec rm -rf {} \;
-#remove empty directory placeholders
 find $RAMFS_TMP -name EMPTY_DIRECTORY -exec rm -rf {} \;
 find $RAMFS_TMP -name .EMPTY_DIRECTORY -exec rm -rf {} \;
 rm -rf $RAMFS_TMP/tmp/* > /dev/null 2>&1
-#remove mercurial repository
 rm -rf $RAMFS_TMP/.hg > /dev/null 2>&1
+
+echo "#################### Build Ramdisk ####################"
 cd $RAMFS_TMP
-find | fakeroot cpio -H newc -o > $RAMFS_TMP.cpio 2>/dev/null
+find . | fakeroot cpio -o -H newc > $RAMFS_TMP.cpio 2>/dev/null
 ls -lh $RAMFS_TMP.cpio
-gzip -9 $RAMFS_TMP.cpio
+gzip -9 -f $RAMFS_TMP.cpio
 
 echo "#################### Compilar Kernel ####################"
-#remove previous out files
+
 rm $KERNELDIR/dt.img > /dev/null 2>&1
 rm $KERNELDIR/boot.img > /dev/null 2>&1
 rm $KERNELDIR/*.ko > /dev/null 2>&1
+
 #compile kernel
 cd $KERNELDIR
-nice -n 10 make -j7 >> compile.log 2>&1 || exit -1
+
+nice -n 10 make -j8 >> compile.log 2>&1 || exit -1
 
 echo "#################### Generar nueva dt image ####################"
 $TOOLBASE/dtbTool -o $KERNELDIR/dt.img -s 2048 -p $KERNELDIR/scripts/dtc/ $KERNELDIR/arch/arm/boot/
@@ -88,9 +100,13 @@ $TOOLBASE/dtbTool -o $KERNELDIR/dt.img -s 2048 -p $KERNELDIR/scripts/dtc/ $KERNE
 echo "#################### Generar nuevo boot.img ####################"
 $TOOLBASE/mkbootimg_dtb --base 0x0 --kernel $KERNELDIR/arch/arm/boot/zImage --ramdisk_offset 0x2000000 --tags_offset 0x1e00000 --pagesize 2048 --cmdline 'console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 zswap.enabled=1 zswap.compressor=lz4' --ramdisk $RAMFS_TMP.cpio.gz --dt $KERNELDIR/dt.img -o $KERNELDIR/boot.img
 
+mkdir -p $ROOTFS_PATH/lib/modules
+mkdir -p $ROOTFS_PATH/system/lib
+ln -s $ROOTFS_PATH/lib/modules/ $ROOTFS_PATH/system/lib
 find . -name "boot.img"
 find . -name "*.ko" -exec mv {} . \;
-${CROSS_COMPILE}strip --strip-unneeded ./*.ko
+find . -name '*.ko' -exec cp -av {} $ROOTFS_PATH/lib/modules/ \;
+${CROSS_COMPILE}strip --strip-unneeded $ROOTFS_PATH/lib/modules/*.ko
 find . -name "*.ko"
 
 echo "#################### Preparando flasheables ####################"
